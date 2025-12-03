3a50a1dcccc102db007ec9e9c278a29fd6c3b512
Saikat Mallick <saikatmallick145@gmail.com>
2025-12-03 23:27:21 +0530

    new files added

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,15 +1,1 @@
-#include<iostream>
-using namespace std;
-void swap(int *a , int *b){
-    int temp=*a;
-    *a=*b;
-    *b=temp;
-}
-int main(){
-    int a,b;
-    cout<<"enter two numbers= "<<endl;
-    cin>>a>>b;
-    swap(&a,&b);
-    cout<<"after swappin the values will be: "<<a<<" "<<b;
-    return 0;
-}
+a raise to power b is= 
